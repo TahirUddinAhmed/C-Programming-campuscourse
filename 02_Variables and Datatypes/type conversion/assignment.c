a23f896c83df73d2288290c38c95eecd55d09af1
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2023-11-28 20:06:14 +0530

    assignment

---
+++ 02_Variables and Datatypes/type conversion/assignment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Given integer and float values
+    int intValue = 10;
+    float floatValue = 5.7;
+
+    // Perform the following tasks:
+    // 1. Convert intValue to float and store it in resultFloat.
+    // 2. Convert floatValue to int and store it in resultInt.
+    // 3. Print all the values
+
+
+    return 0;
+}
