61fc7862ecedf35f75abaf81ab7c2015c2662082
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2023-11-28 20:02:58 +0530

    type conversion

---
+++ 02_Variables and Datatypes/type conversion/code01.c
@@ -0,0 +1,43 @@
+// #include <stdio.h>
+
+// int main() {
+//     // integer to float
+//     float result = (float) 5 / 2;
+
+//     printf("%.1f\n", result);
+
+//     return 0;
+// }
+
+// 2.5
+
+// 2.0000
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int x = 4;  // integer
+    float y = 7.3; // float
+
+    printf("%f\n", x + y);
+    return 0;
+}
+
+// 11.300000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 02_Variables and Datatypes/type conversion/code02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // integer to character
+    // char ch = 64; // a
+
+
+    // float to integer
+    float a = 3.1;
+    int b = a;
+
+    // explicit conversion
+    float f = 13.4;
+    int i = (int) f;
+
+    // print -> ch
+    printf("%d\n", (int) f);
+    return 0;
+}
